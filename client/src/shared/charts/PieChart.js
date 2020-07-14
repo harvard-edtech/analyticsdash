3e5e4cd51306f5d3e09b4a54bc24e58e7ce5c5f0
Gabe Abrams <gabeabrams@gmail.com>
2020-07-14 12:09:41 -0400

    Added skeleton code for charts

---
+++ client/src/shared/charts/PieChart.js
@@ -0,0 +1,64 @@
+/**
+ * Pie chart displayed as a doughnut chart
+ * @author Gabe Abrams
+ */
+
+// Import React
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Import FontAwesome Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+
+class PieChart extends Component {
+  /**
+   * Render PieChart
+   * @author Gabe Abrams
+   */
+  render() {
+    return (
+      <div>
+        PieChart still being build!
+      </div>
+    );
+  }
+}
+
+PieChart.propTypes = {
+  // Title of the chart
+  title: PropTypes.string.isRequired,
+  // Segment data
+  segments: PropTypes.arrayOf(PropTypes.shape({
+    // Label of the segment
+    label: PropTypes.string.isRequired,
+    // The value for the segment
+    value: PropTypes.number,
+  })).isRequired,
+  // Segment label type
+  seriesLabelType: PropTypes.oneOf([
+    'legend', // Show a legend
+    'inner', // Show labels inside the segments
+    'outer', // Show labels outside the segments
+  ]),
+  // True to show segment values
+  showSegmentValues: PropTypes.bool,
+  /**
+   * Tooltip formatter
+   * @param {number} value - the value of the bar series item
+   * @param {string} label - the label of the segment
+   * @return {node} valid html element
+   */
+  tooltipFormatter: PropTypes.func,
+};
+
+PieChart.defaultProps = {
+  // Outside the segments
+  seriesLabelType: 'outer',
+  // Values hidden
+  showSegmentValues: false,
+  // No tooltip formatter
+  tooltipFormatter: undefined,
+};
+
+export default PieChart;
---
+++ client/src/shared/charts/style/THEMES.js
@@ -0,0 +1,16 @@
+/**
+ * List of themes (array of pairs of colors and textures)
+ */
+export default {
+  default: [
+    {
+      color: 'red',
+      texture: 'dots',
+    },
+    ...
+  ],
+  'good-bad': [
+    ...
+  ],
+  ...
+};
