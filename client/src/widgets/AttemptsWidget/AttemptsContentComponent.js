70f006c0c21d38ffa7345c27c630a2c717b5b834
apandey00 <aryanpnd74@gmail.com>
2020-07-15 06:16:25 -0400

    prelim chart

--- client/src/widgets/AttemptsWidget/AttemptsContentComponent.js
+++ client/src/widgets/AttemptsWidget/AttemptsContentComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 /**
  * Content for the Attempts component
  * @author Aryan Pandey
@@ -6,11 +7,8 @@
 // Import React
 import React, { Component } from 'react';
 
-// Import caccl
-import initCACCL from 'caccl/client/cached';
-
 // Import chart component
-import { ResponsiveBar } from '@nivo/bar'
+import { ResponsiveBar } from '@nivo/bar';
 
 // Import shared components
 import AssignmentsDropdown from '../../shared/AssignmentsDropdown';
@@ -18,11 +16,6 @@ import LoadingSpinner from '../../shared/LoadingSpinner';
 
 // Get data
 import getCanvasData from '../../helpers/getCanvasData';
-
-/* ----------------------- Initialization ----------------------- */
-
-// Initialize caccl
-const { api } = initCACCL();
 
 /* ---------------------------- Class --------------------------- */
 
@@ -49,76 +42,73 @@ class AttemptsContentComponent extends Component {
       assignment,
     } = this.state;
 
-    const {numStudents} = this;
+    const { numStudents } = this;
 
     // Initialize empty buckets for bar chart
     const data = [
       {
-        "attempts": "No Submissions",
-        "students": 0
+        attempts: 'No Submissions',
+        students: 0,
       },
       {
-        "attempts": "1",
-        "students": 50
+        attempts: '1',
+        students: 0,
       },
       {
-        "attempts": "2",
-        "students": 0
+        attempts: '2',
+        students: 0,
       },
       {
-        "attempts": "3",
-        "students": 0
+        attempts: '3',
+        students: 0,
       },
       {
-        "attempts": "4",
-        "students": 0
+        attempts: '4',
+        students: 0,
       },
       {
-        "attempts": "4+",
-        "students": 0
-      }
-    ]
+        attempts: '4+',
+        students: 0,
+      },
+    ];
 
-    /* ---------------------------- Assignment Dropdown ---------------------------- */
-    
-        // Create assignment chooser
-        const assignmentDropdown = (
-          <AssignmentsDropdown
-            onChange={(newAssignment) => {
-              this.setState({
-                assignment: newAssignment,
-              });
-            }}
-          />
-        );
-    
+    /* ------------------------ Assignment Dropdown ------------------------ */
+
+    // Create assignment chooser
+    const assignmentDropdown = (
+      <AssignmentsDropdown
+        onChange={(newAssignment) => {
+          this.setState({
+            assignment: newAssignment,
+          });
+        }}
+      />
+    );
+
     if (numStudents && assignment) {
       const submissionsList = getCanvasData().listSubmissions(assignment.id);
 
       submissionsList.forEach((value) => {
-        // TODO?: Check if value is a valid number
-
         // extract attempts data
         const { attempt } = value;
         if (attempt > 4) {
-          data[4].students++;
+          data[4].students += 1;
         }
-        data[attempt].students++;
-      })
+        data[attempt].students += 1;
+      });
     }
 
     // custtom tooltip that appears on hover
     const customTooltip = (args) => {
-      console.log(args)
-      let percentage = (( args.value / numStudents ) * 100).toFixed(2);
+      const percentage = ((args.value / numStudents) * 100).toFixed(2);
       return (
         <div>
           <strong>{args.value} </strong>
-          students (<strong>{percentage}%</strong>) used {args.indexValue} attempt(s)
+          students ( <strong> {percentage}% </strong> ) used {args.index} attempt(s)
         </div>
       );
-    }
-        const getColor = (bar) => { return bar.indexValue === 'No Submissions' ? '#FF3333' : '#03A9F3' }
+    };
+    const getColor = (bar) => { return bar.indexValue === 'No Submissions' ? '#DCDCDC' : '#03A9F3'; };
 
     // initialize the bar chart
     const bar = (
@@ -126,15 +116,18 @@ class AttemptsContentComponent extends Component {
         data={data}
         keys={['students']}
         indexBy="attempts"
-        maxValue={numStudents}
-        margin={{ top: 20, right: 80, bottom: 50, left: 80 }}
+
+        margin={{
+          top: 20, right: 80, bottom: 50, left: 80,
+        }}
         padding={0.3}
         colors={getColor}
+        maxValue={numStudents}
         borderColor={{
           from: 'color',
           modifiers: [
-            ['darker', .6],
-          ]
+            ['darker', 0.6],
+          ],
         }}
         borderRadius={1}
         axisLeft={{
@@ -160,25 +153,24 @@ class AttemptsContentComponent extends Component {
           axis: {
             ticks: {
               text: {
-                fill: "#aaaaaa",
-              }
+                fill: '#aaaaaa',
+              },
             },
             legend: {
               text: {
-                fill: "#333333",
+                fill: '#333333',
                 fontSize: '20px',
-              }
+              },
             },
-          }
-        }
-        }
+          },
+        }}
         labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-        animate={true}
+        animate
         motionStiffness={90}
         motionDamping={15}
         tooltip={customTooltip}
       />
-    )
+    );
 
     /* ---------------------------- Body ---------------------------- */
 
@@ -197,7 +189,7 @@ class AttemptsContentComponent extends Component {
       <div>
         {assignmentDropdown}
         {body}
-        <div style={{ width: '100%', height: '500px' , paddingBottom: '10px' }}>
+        <div style={{ width: '100%', height: '500px', paddingBottom: '10px' }}>
           {bar}
         </div>
       </div>
