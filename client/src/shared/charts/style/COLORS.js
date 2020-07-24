85562d05f3f405ac4ce47ec6a1cbcc5e9cb391ed
Grace Whitney <g_whitney@college.harvard.edu>
2020-07-24 13:25:25 -0400

    added pattern colors to color objects

--- client/src/shared/charts/style/COLORS.js
+++ client/src/shared/charts/style/COLORS.js
@@ -6,46 +6,57 @@
 export default {
   BLUE: {
     id: 'blue',
-    background: '33A0E4',
+    background: '#33A0E4',
+    color: '#136090',
   },
   ORANGE: {
     id: 'orange',
-    background: 'F39A27',
+    background: '#F39A27',
+    color: '#AE670A',
   },
   PURPLE: {
     id: 'purple',
-    background: 'A981D4',
+    background: '#A981D4',
+    color: '#783EB6',
   },
   GREEN: {
     id: 'green',
-    background: '33B16D',
+    background: '#33B16D',
+    color: '#206F45',
   },
   LIGHT_BLUE: {
     id: 'lightBlue',
-    background: 'B6E9F9',
+    background: '#B6E9F9',
+    color: '#68D2F3',
   },
   RED: {
     id: 'red',
-    background: 'F0454F',
+    background: '#F0454F',
+    color: '#BD0F1B',
   },
   YELLOW: {
     id: 'yellow',
-    background: 'FFF771',
+    background: '#FFF771',
+    color: '#DDDDDD',
   },
   PINK: {
     id: 'pink',
-    background: 'FFD1E9',
+    background: '#FFD1E9',
+    color: '#FF99CE',
   },
   TURQUOISE: {
     id: 'turquoise',
-    background: '00D1E1',
+    background: '#00D1E1',
+    color: '#00858F',
   },
   LIGHT_GREEN: {
     id: 'lightGreen',
-    background: 'B4E9A9',
+    background: '#B4E9A9',
+    color: '#71D55D',
   },
   GRAY: {
     id: 'gray',
     background: 'DADEE3',
-  }
+    color: 'CCCCCC',
+  },
 };
