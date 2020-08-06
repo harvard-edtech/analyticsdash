c5c3c8e104ae6baa002bb40c8bae8657d8fbc66d
Grace Whitney <g_whitney@college.harvard.edu>
2020-08-06 12:25:02 -0400

    Updated GradeHistogram widget to used shared bar chart component

--- client/src/widgets/GradeHistogram/GradeHistogramContentComponent.js
+++ client/src/widgets/GradeHistogram/GradeHistogramContentComponent.js
@@ -13,6 +13,7 @@ import { ResponsiveBar } from '@nivo/bar';
 // Import shared components
 import AssignmentsDropdown from '../../shared/AssignmentsDropdown';
 import LoadingSpinner from '../../shared/LoadingSpinner';
+import BarChart from '../../shared/charts/BarChart';
 
 // Import style
 import './GradeHistogramContentComponent.css';
@@ -100,7 +101,7 @@ class GradeHistogramContentComponent extends Component {
         const histogramData = [];
         for (let i = 0; i < numBuckets; i++) {
           const min = Number((bucketSize * i).toFixed(2));
-          const max = Number((bucketSize * (i + 1)).toFixed(2) - 0.01);
+          const max = Number((bucketSize * (i + 1) - 0.01).toFixed(2));
 
           // First and last buckets have alternate label format
           let label;
@@ -114,7 +115,7 @@ class GradeHistogramContentComponent extends Component {
 
           histogramData.push({
             label,
-            numSubmissions: 0,
+            value: 0,
           });
         }
         scores.forEach((score) => {
@@ -126,50 +127,25 @@ class GradeHistogramContentComponent extends Component {
           } else {
             bucketIndex = Math.floor(score / bucketSize);
           }
-          histogramData[bucketIndex].numSubmissions += 1;
+          histogramData[bucketIndex].value += 1;
         });
 
         body = (
-          <div className="GradeHistogramContentComponent-body-container">
-            <ResponsiveBar
-              data={histogramData}
-              indexBy="label"
-              keys={['numSubmissions']}
-              padding={0.05}
-              groupMode="grouped"
-              margin={{
-                top: 50,
-                right: 50,
-                bottom: 50,
-                left: 50,
-              }}
-              label="label"
-              enableGridY
-              axisLeft={{
-                legend: 'Number of Submissions',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legendPosition: 'middle',
-                legendOffset: -40,
-              }}
-              axisBottom={{
-                legend: 'Grade Range',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legendPosition: 'middle',
-                legendOffset: 40,
-              }}
-              tooltip={(bar) => {
-                return (
-                  <div>
-                    {`${bar.value} submissions (${((bar.value / subs.length) * 100).toFixed(2)}%)`}
-                  </div>
-                );
-              }}
-            />
-          </div>
+          <BarChart
+            title="Grade Distribution"
+            valueAxisLabel="Number of Submissions"
+            barAxisLabel="Grade Range"
+            lessPaddingBetweenBars
+            bars={histogramData}
+            padding={0.05}
+            tooltip={(bar) => {
+              return (
+                <div>
+                  {`${bar.value} submissions (${((bar.value / subs.length) * 100).toFixed(2)}%)`}
+                </div>
+              );
+            }}
+          />
         );
       }
     }
