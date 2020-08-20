d26ad7ead27e8592b73459b596b15a8690a808ce
Grace Whitney <g_whitney@college.harvard.edu>
2020-08-20 17:24:33 -0400

    Added onClose props

--- client/src/shared/MessageStudentsModal.js
+++ client/src/shared/MessageStudentsModal.js
@@ -20,6 +20,16 @@ class MessageStudentsModal extends Component {
 
     this.state = {
       state: STATES.PREVIEW,
+    };
+  }
+
+  sendMessage() {
+    this.setState({ state: STATES.LOADING });
+    try {
+      // Send message here
+      this.setState({ state: STATES.SUCCESS });
+    } catch (err) {
+      this.setState({ state: STATES.FAILURE });
     }
   }
 
@@ -35,6 +45,14 @@ class MessageStudentsModal extends Component {
       onClose,
     } = this.props;
 
+    const onClosePreview = (button) => {
+      if (button === Modal.BUTTONS.OKAY) {
+        return this.sendMessage();
+      }
+      // Otherwise, close modal
+      return onClose();
+    };
+
     const previewView = (
       <div />
     );
@@ -44,7 +62,9 @@ class MessageStudentsModal extends Component {
     );
 
     const successView = (
-      <div />
+      <div>
+        The message was sent successfully!
+      </div>
     );
 
     const failureView = (
@@ -53,24 +73,29 @@ class MessageStudentsModal extends Component {
 
     let body;
     let type;
+    let onCloseView;
     if (state === STATES.PREVIEW) {
       body = previewView;
       type = Modal.TYPES.OKAY_CANCEL;
+      onCloseView = onClosePreview;
     } else if (state === STATES.LOADING) {
       body = loadingView;
       type = Modal.TYPES.BLOCKED;
     } else if (state === STATES.SUCCESS) {
       body = successView;
       type = Modal.TYPES.NO_BUTTONS;
+      onCloseView = onClose;
     } else if (state === STATES.FAILURE) {
       body = failureView;
       type = Modal.TYPES.NO_BUTTONS;
+      onCloseView = onClose;
     }
 
     return (
       <Modal
+        key={`message-students-modal-${state}`}
         body={body}
-        onClose={onClose}
+        onClose={onCloseView}
         title="Message Preview"
         type={type}
         okayLabel="Send Message"
@@ -83,7 +108,12 @@ MessageStudentsModal.propTypes = {
   recipientIds: PropTypes.arrayOf(PropTypes.number).isRequired,
   subject: PropTypes.string.isRequired,
   defaultBody: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
-}
+  onClose: PropTypes.func,
+};
+
+MessageStudentsModal.defaultProps = {
+  // No handler
+  onClose: () => {},
+};
 
 export default MessageStudentsModal;
