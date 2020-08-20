f093aa6cd1228675f0ba2e8c218ac9626863849b
apandey00 <aryanpnd74@gmail.com>
2020-08-20 09:00:11 -0400

    updated tooltip component

--- client/src/shared/Tooltip.js
+++ client/src/shared/Tooltip.js
@@ -11,6 +11,9 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
+// Import tippy tooltip library
+import Tippy from '@tippyjs/react';
+
 // Import styles
 import './Tooltip.css';
 
@@ -33,28 +36,54 @@ class Tooltip extends Component {
         className="Tooltip-help-button btn-xsm btn-info"
         onClick={onOpenHelp}
       >
-        Show More
+        {/* Switch displays depending on screen size */}
+        <span className="d-none d-md-block">
+          Show More
+        </span>
+        <span className="d-sm-block d-md-none">
+          More
+        </span>
       </button>
     );
 
-    // return tooltip component
-    return (
-      <div className="Tooltip-circle text-secondary">
-        <FontAwesomeIcon
-          icon={faQuestionCircle}
-        />
-        <div className="Tooltip-container">
-          <div className="Tooltip-help-bar bg-secondary">
-            Help
-            {
-              // include helpButton only if onOpenHelp defined
-              (onOpenHelp ? helpButton : null)
-            }
-          </div>
-          <div className="Tooltip-text">
-            {text}
-          </div>
+    // Html element that shows up on tooltip hover
+    const tooltipContentElement = (
+      <div className="Tooltip-container">
+        <div className="Tooltip-help-bar bg-secondary">
+          Help
+          {
+            // include helpButton only if onOpenHelp defined
+            (onOpenHelp ? helpButton : null)
+          }
         </div>
+        <div className="Tooltip-text">
+          {text}
+        </div>
+      </div>
+    );
+
+    // return tippy tooltip component
+    return (
+      <div style={{ display: 'inline' }}>
+        <Tippy
+          theme="transparent"
+          placement="auto"
+          content={tooltipContentElement}
+          interactive
+          allowHTML
+          distance={5}
+          interactiveBorder={5}
+          hideOnClick={false}
+        >
+          <button
+            type="button"
+            className="Tooltip-circle"
+          >
+            <FontAwesomeIcon
+              icon={faQuestionCircle}
+            />
+          </button>
+        </Tippy>
       </div>
     );
   }
