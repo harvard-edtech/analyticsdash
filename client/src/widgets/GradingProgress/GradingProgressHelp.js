d6fb9c9e22598b9a457eef7c4ff69d2be22e6262
Grace Whitney <g_whitney@college.harvard.edu>
2020-08-28 17:00:25 -0400

    Updated help view content

--- client/src/widgets/GradingProgress/GradingProgressHelp.js
+++ client/src/widgets/GradingProgress/GradingProgressHelp.js
@@ -10,7 +10,8 @@ class GradingProgressHelp extends Component {
   render() {
     return (
       <div>
-        No additional help available for this component.
+        Use the CSV download buttons to receive detailed data about individual
+        {' student submissions\' grading status.'}
       </div>
     );
   }
