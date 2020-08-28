c09ff735e77747f3dbda28319f37d0e85cd09dd5
Grace Whitney <g_whitney@college.harvard.edu>
2020-08-28 16:49:16 -0400

    Added grading progress help skeleton

---
+++ client/src/widgets/GradingProgress/GradingProgressHelp.js
@@ -0,0 +1,19 @@
+/**
+ * Help component for the Grading Progress widget
+ * @author Grace Whitney
+ */
+
+// Import React
+import React, { Component } from 'react';
+
+class GradingProgressHelp extends Component {
+  render() {
+    return (
+      <div>
+        No additional help available for this component.
+      </div>
+    );
+  }
+}
+
+export default GradingProgressHelp;
