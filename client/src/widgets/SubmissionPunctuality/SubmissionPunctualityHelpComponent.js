ba2969218d14aca0a8f2f535827e50f6ffda5b48
apandey00 <aryanpnd74@gmail.com>
2020-08-29 11:35:28 -0400

    Help Component

---
+++ client/src/widgets/SubmissionPunctuality/SubmissionPunctualityHelpComponent.js
@@ -0,0 +1,23 @@
+/**
+ * Help component content for Submission Punctuality widget
+ * @author Aryan Pandey
+ */
+
+import React, { Component } from 'react';
+
+/* ---------------------------- Class --------------------------- */
+class SubmissionPunctualityHelpComponent extends Component {
+  /**
+   * Render SubmissionPunctualityHelp
+   * @author Aryan Pandey
+   */
+  render() {
+    return (
+      <div>
+        This widget provides info about assignment submission punctuality.
+      </div>
+    );
+  }
+}
+
+export default SubmissionPunctualityHelpComponent;
